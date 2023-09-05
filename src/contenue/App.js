c22fe38a04345b756e807c50dd18ccf50cf9fc45
samar <samarsaidani10@gmail.com>
2023-09-05 18:19:35 +0200

    new

---
+++ src/contenue/App.js
@@ -0,0 +1,20 @@
+
+import NavScrollExample from './navision';
+import Car from './cart';
+
+
+function Application1(){
+    return(
+        <div>
+            <NavScrollExample/>
+            <br></br>
+           <div class="carte"> <Car/>
+            <Car/>
+            <Car/></div>
+            <br></br>
+            
+
+        </div>
+    )
+}
+export default Application1;
---
+++ src/contenue/cart.js
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Car() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_q6iheLc0wxg9jaSari5k_V7yNcBe63k6Ow&usqp=CAU" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Car;
+
+
+
